ccdd943f96f8aaed5187f192d166dd239e5f2d4c
Mehmet Keçeci <mehmet.42.kececi@gmail.com>
2024-04-29 14:43:21 +0300

    init

---
+++ app/src/main/java/handicraft/app/AppApplication.java
@@ -0,0 +1,13 @@
+package handicraft.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
