06f05b930abe636b756022c93424469ee70fd6c0
Mehmet Keçeci <mehmet.42.kececi@gmail.com>
2024-05-05 16:58:53 +0300

    instructorAttends relation

--- handicraft/src/main/java/app/handicraft/model/handicraft/Handicraft.java
+++ handicraft/src/main/java/app/handicraft/model/handicraft/Handicraft.java
@@ -1,5 +1,7 @@
 package app.handicraft.model.handicraft;
 
+import app.handicraft.model.relation.InstructorAttends;
+import app.handicraft.model.user.Instructor;
 import jakarta.persistence.*;
 
 import java.util.ArrayList;
@@ -15,9 +17,22 @@ public class Handicraft {
     @JoinColumn(name = "handicraft_type_id")
     private HandicraftType handicraftType;
 
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "instructor_attends_id")
+    private List<InstructorAttends> instructorAttends;
+
     public Handicraft(Float fee, HandicraftType handicraftType) {
         this.fee = fee;
         this.handicraftType = handicraftType;
+        this.instructorAttends = new ArrayList<>();
+    }
+
+    public List<InstructorAttends> getInstructorAttends() {
+        return instructorAttends;
+    }
+
+    public void setInstructorAttends(List<InstructorAttends> instructorAttends) {
+        this.instructorAttends = instructorAttends;
     }
 
     public UUID getId() {
--- handicraft/src/main/java/app/handicraft/model/user/Instructor.java
+++ handicraft/src/main/java/app/handicraft/model/user/Instructor.java
@@ -4,6 +4,7 @@ import app.handicraft.model.course.Course;
 import app.handicraft.model.handicraft.Handicraft;
 import app.handicraft.model.handicraft.HandicraftType;
 import app.handicraft.model.relation.ApplicantAttends;
+import app.handicraft.model.relation.InstructorAttends;
 import jakarta.persistence.*;
 
 import java.util.ArrayList;
@@ -14,9 +15,21 @@ public class Instructor extends User {
     @OneToMany(fetch = FetchType.LAZY)
     @JoinColumn(name = "handicraft_type_list")
     private List<HandicraftType> handicraftTypeList;
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "instructor_attends_id")
+    private List<InstructorAttends> instructorAttends;
     public Instructor(String userName, String name, String surname, String eMail, String phoneNumber, String address) {
         super(userName, name, surname, eMail, phoneNumber, address);
         this.handicraftTypeList = new ArrayList<>();
+        this.instructorAttends = new ArrayList<>();
+    }
+
+    public List<InstructorAttends> getInstructorAttends() {
+        return instructorAttends;
+    }
+
+    public void setInstructorAttends(List<InstructorAttends> instructorAttends) {
+        this.instructorAttends = instructorAttends;
     }
 
     public List<HandicraftType> getHandicraftTypeList() {
