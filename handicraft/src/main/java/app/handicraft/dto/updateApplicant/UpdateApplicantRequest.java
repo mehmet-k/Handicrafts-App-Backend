d4f81dbad5ccdfb9f4afcb01b95df1c672f5f547
Emir Çağrı Aykın <emircagriaykin@gmail.com>
2024-05-09 15:37:27 +0300

    test

--- handicraft/src/main/java/app/handicraft/dto/updateApplicant/UpdateApplicantRequest.java
+++ handicraft/src/main/java/app/handicraft/dto/updateApplicant/UpdateApplicantRequest.java
@@ -1,13 +1,11 @@
 package app.handicraft.dto.updateApplicant;
 
-import java.util.UUID;
-
 public record UpdateApplicantRequest(
         String userName,
         String name,
         String surname,
         String eMail,
         String phoneNumber,
-        String address
-) {
+        String address,
+        String email) {
 }
