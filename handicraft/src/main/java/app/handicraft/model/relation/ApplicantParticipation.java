4f0b3797c1d43b794d63441b33e9fb4b764f5948
Mehmet Keçeci <mehmet.42.kececi@gmail.com>
2024-05-14 13:11:21 +0300

    Update ApplicantParticipation.java

--- handicraft/src/main/java/app/handicraft/model/relation/ApplicantParticipation.java
+++ handicraft/src/main/java/app/handicraft/model/relation/ApplicantParticipation.java
@@ -26,6 +26,9 @@ public class ApplicantParticipation {
         this.attendanceStatus = attendanceStatus;
     }
 
+    public ApplicantParticipation() {
+    }
+
     public UUID getId() {
         return id;
     }
